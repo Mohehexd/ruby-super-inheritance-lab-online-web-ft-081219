9407dcdb77aba86fc18a2925bdb74e8a82066ce5
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-08 19:55:12 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,15 +7,15 @@ class ChattyStudent
   end
   
   def raise_hand 
-    super 
-    super 
-    super 
-    super 
-    super 
-    super 
-    super 
-    super
-    super
-    super 
+    super(10)
+    # super 
+    # super 
+    # super 
+    # super 
+    # super 
+    # super 
+    # super
+    # super
+    # super 
   end 
 end 
